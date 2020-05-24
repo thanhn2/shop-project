aa2b616d05496ba1f26ace1b6286665bfcd89d07
ThanhN2 <nguyenthanh090493@gmail.com>
2020-05-24 23:50:25 +0700

    ThanhN2_Add API for shop project

--- api/src/main/java/com/shop/api/controller/ProductController.java
+++ api/src/main/java/com/shop/api/controller/ProductController.java
@@ -1,19 +1,41 @@
 package com.shop.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.shop.api.entity.Product;
+import com.shop.api.exception.BusinessException;
 import com.shop.api.service.ProductService;
 
-@Controller
+@RestController
+@RequestMapping(path = "/api")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
-    @RequestMapping(path = "/index")
-    public String index() {
-        return "index";
+    @RequestMapping(path = "/search/{name}")
+    @ResponseBody
+    public Object search(String name) {
+        try {
+            return productService.findByNameContaining(name);
+        } catch (BusinessException e) {
+            return e;
+        }
     }
+
+    @RequestMapping(path = "/save")
+    @ResponseBody
+    public Object save(@RequestBody Product product) {
+        try {
+            return HttpStatus.CREATED;
+        } catch (BusinessException e) {
+            return e;
+        }
+    }
+
 }
--- api/src/main/java/com/shop/api/service/ProductServiceImpl.java
+++ api/src/main/java/com/shop/api/service/ProductServiceImpl.java
@@ -3,9 +3,11 @@ package com.shop.api.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.shop.api.entity.Product;
+import com.shop.api.exception.BusinessException;
 import com.shop.api.repository.ProductRepository;
 
 @Service
@@ -16,12 +18,20 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List<Product> findByNameContaining(String name) {
-        return productRepository.findByNameContaining(name);
+        try {
+            return productRepository.findByNameContaining(name);
+        } catch (Exception e) {
+            throw new BusinessException("", e, HttpStatus.NOT_FOUND);
+        }
     }
 
     @Override
     public void save(Product contact) {
-        productRepository.save(contact);
+        try {
+            productRepository.save(contact);
+        } catch (Exception e) {
+            throw new BusinessException("", e, HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
