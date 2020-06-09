3a98404767a9eb3e23feef21c18446d8b0af085a
Nhat <you@exaple.com>
2020-06-09 18:25:46 +0700

    Commit template

---
+++ front-ent/src/components/Cart.vue
@@ -0,0 +1,46 @@
+<template>
+  <aside class="col-md-4 blog-sidebar">
+    <div class="p-4 mb-3 bg-light rounded d-flex justify-content-between">
+      <b class="font-italic my-2">Your cart:</b>
+      <button
+        type="button"
+        class="btn btn-outline-primary"
+        data-toggle="modal"
+        data-target="#buyAllModal"
+      >Buy all</button>
+    </div>
+
+    <div class="p-4">
+      <ol class="list-unstyled mb-0 list-buy">
+        <li class="d-flex justify-content-between">
+          <span>
+            <a>&#8864;</a> March 2014
+          </span>
+          <span>10000</span>
+        </li>
+        <li>March 2014</li>
+        <li>March 2014</li>
+      </ol>
+      <hr />
+      <div class="d-flex justify-content-between">
+        <span>Thanh tien:</span>
+        <b class="paymoney">1.490.000₫</b>
+      </div>
+    </div>
+
+    <!-- Modal buy all -->
+  </aside>
+  <!-- /.blog-sidebar -->
+</template>
+
+<script>
+// import Buy-Detail from "./Buy-Detail.vue";
+
+export default {
+  name: "Cart",
+  props: {
+    msg: String
+  }
+  // components: {}
+};
+</script>
---
+++ front-ent/src/components/Search.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3 class="pb-1 font-italic border-bottom">From the Firehose</h3>
+    <div class="input-group mb-3">
+      <input type="text" class="form-control" placeholder="Search..." />
+      <div class="input-group-append">
+        <button class="btn btn-outline-secondary" type="button">X</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Search",
+  props: {
+    msg: String
+  }
+};
+</script>
